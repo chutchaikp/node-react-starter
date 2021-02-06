a643bf877e5718574a75baa2f8bab22ebf4f1ccd
chutchai.kp <chutchai.kp@gmail.com>
2021-02-07 03:51:11 +0700

    Update Redux Vanila

--- react-redux-vanila/src/App.js
+++ react-redux-vanila/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { combineReducers, createStore, } from 'redux';
 import { Provider, useSelector } from 'react-redux';
 import './App.css';
+import axios from 'axios';
 
 var store = {};
 
@@ -28,6 +29,11 @@ function App() {
         return state.filter((u) => {
           return u !== action.payload;
         });
+      case 'LOAD_NAMES':
+        const data = action.payload.map((n) => n.name);
+        return [...state, ...data];
+      case 'CLEAR_NAMES':
+        return [];
       default:
         return state;
     }
@@ -37,20 +43,23 @@ function App() {
     counter,
     names,
   });
+
   store = createStore(allReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
   return (
     <Provider store={store}>
-      <div className="App">
-        <h1> Basic Redux </h1>
-        <div className="column">
-          <ComponentA />
+
+      <div className="container">
+        <div className="header">
+          {store && store.getState && <Header />}
         </div>
-        <div className="column">
-          {store && store.getState && <ComponentB />}
+
+        <div className="content">
+          <AppContent />
         </div>
+
       </div>
     </Provider>
   );
@@ -58,29 +67,33 @@ function App() {
 
 export default App;
 
-const ComponentA = () => {
-  const names = useSelector(state => state.names);
 
-  const [localCounter, setLocalCounter] = useState(0);
+const AppContent = () => {
+  const [counter, names] = useSelector(state => {
+    return [state.counter, state.names]
+  });
+
   const [localName, setLocalName] = useState('A');
   return (
     <div>
-      <h2>Component A</h2>
+
       <h3>Counter</h3>
-      <p>{localCounter}</p>
-      <button onClick={() => {
-        store.dispatch({ type: 'INCREMENT' });
-        setLocalCounter(store.getState().counter);
-      }} >+</button>
-      <button onClick={() => {
-        store.dispatch({ type: 'DECREMENT' });
-        setLocalCounter(store.getState().counter);
-      }} >-</button>
+      <p>{counter}</p>
+      <button onClick={() => store.dispatch({ type: 'INCREMENT' })} >+</button>
+      <button onClick={() => store.dispatch({ type: 'DECREMENT' })} >-</button>
       <hr />
       <h3>Names</h3>
       <input type="text" value={localName} onChange={(e) => setLocalName(e.target.value)} />
       <button onClick={() => store.dispatch({ type: 'INSERT_NAME', payload: localName })}>Add</button>
       <button onClick={() => store.dispatch({ type: 'DELETE_NAME', payload: localName })}>Delete</button>
+
+      <button onClick={() => store.dispatch({ type: 'CLEAR_NAMES' })}>Clear</button>
+
+      {/* Do Async  */}
+      <button className="asyncButton" onClick={async () => {
+        const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+        store.dispatch({ type: 'LOAD_NAMES', payload: data });
+      }}>Load Async</button>
 
       <ul>
         {names.map((n, idx) => {
@@ -92,24 +105,25 @@ const ComponentA = () => {
   );
 }
 
-const ComponentB = () => {
+const Header = () => {
   const [counter, names] = useSelector(state => {
     return [state.counter, state.names]
   });
 
   return (
-    <div>
-      <h2>Component B</h2>
-      <h3>Counter</h3>
-      <p>{counter}</p>
-      <p>{store.getState().counter}</p>
-      <hr />
-      <h3>Names</h3>
-      <ul>
+    <div className="header-wrapper">
+      <h2>Header</h2>
+      <div>
+        <p>Counter {counter}</p>
+        <p>Names {names.length}</p>
+      </div>
+
+      {/* <p>{store.getState().counter}</p>       */}
+      {/* <ul>
         {names.map((n, idx) => {
           return (<li key={idx}>{n}</li>)
         })}
-      </ul>
+      </ul> */}
     </div>
   );
 }
